4ae2df02441f858ebbc39cc3de742e316a553664
SirBatoio <116562702+SirBatoio@users.noreply.github.com>
2022-10-26 17:01:12 +0200

    punti haha

--- app/src/main/java/com/example/mnemosine_allena_la_memoria/OrientamentoTemporale.java
+++ app/src/main/java/com/example/mnemosine_allena_la_memoria/OrientamentoTemporale.java
@@ -34,7 +34,7 @@ public class OrientamentoTemporale extends AppCompatActivity {
     private boolean volume=true;
     private static final long TEMPO = 2000;
     private int i=-1, l=10, tentativi=2;
-    private int pt_totalizzati=0, pt_max;
+    private int pt_totalizzati=1, pt_max;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
