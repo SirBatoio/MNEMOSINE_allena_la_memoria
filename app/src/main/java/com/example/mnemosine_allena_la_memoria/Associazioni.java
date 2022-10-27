9c8ef71633a84285290032c15e12dfd11160c07e
SirBatoio <116562702+SirBatoio@users.noreply.github.com>
2022-10-27 13:28:37 +0200

    bo

--- app/src/main/java/com/example/mnemosine_allena_la_memoria/Associazioni.java
+++ app/src/main/java/com/example/mnemosine_allena_la_memoria/Associazioni.java
@@ -16,6 +16,7 @@ public class Associazioni extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_associazioni);
+
     }
 
     public void cambiaVolume(@NonNull View v)
