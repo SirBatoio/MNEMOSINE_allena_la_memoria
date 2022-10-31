fff669b7d6c9895edf91157450ba6557e497b21c
SirBatoio <116562702+SirBatoio@users.noreply.github.com>
2022-10-31 16:19:32 +0100

    Update Sequenze.java

--- app/src/main/java/com/example/mnemosine_allena_la_memoria/Sequenze.java
+++ app/src/main/java/com/example/mnemosine_allena_la_memoria/Sequenze.java
@@ -3,6 +3,7 @@ package com.example.mnemosine_allena_la_memoria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Sequenze extends AppCompatActivity {
 
