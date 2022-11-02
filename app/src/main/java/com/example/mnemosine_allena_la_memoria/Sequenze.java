1933f40d44a097cb7f4cf42fd8c519abde0b3281
SirBatoio <116562702+SirBatoio@users.noreply.github.com>
2022-11-02 14:08:28 +0100

    AAAAAAAAAArrayList<>

--- app/src/main/java/com/example/mnemosine_allena_la_memoria/Sequenze.java
+++ app/src/main/java/com/example/mnemosine_allena_la_memoria/Sequenze.java
@@ -17,10 +17,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Sequenze extends AppCompatActivity {
-    private ImageView seq_1, seq_2, seq_3,risp_1,risp_2,risp_3,immagine2;
+    private ImageView immagine2;
     private Bitmap cerchio_bianco, cerchio_nero, cerchio_mezzo, quadrato_bianco, quadrato_nero, quadrato_mezzo, triangolo_bianco, triangolo_nero, triangolo_mezzo, freccia_bianco, freccia_nero;
     private ArrayList<ImageView> sequenza = new ArrayList(), risposte = new ArrayList();
-    private ArrayList<Immagine> immagini_seq = new ArrayList(), immagini_risp = new ArrayList();
+    private ArrayList<Immagine> immagini_seq1 = new ArrayList(), immagini_seq2 = new ArrayList(), immagini_seq3 = new ArrayList(), immagini_seq4 = new ArrayList(), immagini_seq5 = new ArrayList(), immagini_seq6 = new ArrayList(), immagini_seq7 = new ArrayList(), immagini_seq8 = new ArrayList(), immagini_seq9 = new ArrayList(), immagini_seq10 = new ArrayList(), immagini_seq11 = new ArrayList(), immagini_seq12 = new ArrayList(), immagini_seq13 = new ArrayList(), immagini_seq14 = new ArrayList(), immagini_risp = new ArrayList();
     private ArrayList<ArrayList<Immagine>> domande = new ArrayList();
     private MediaPlayer mp;
     private static final long TEMPO = 2000;
@@ -60,39 +60,30 @@ public class Sequenze extends AppCompatActivity {
 
     public void generaDomande()
     {
-        immagini_seq.add(new Immagine(cerchio_bianco, 0));
-        immagini_seq.add(new Immagine(cerchio_nero, 0));
-        immagini_seq.add(new Immagine(quadrato_bianco, 0));
-        immagini_seq.add(new Immagine(quadrato_nero, 0));
+        immagini_seq1.add(new Immagine(cerchio_bianco, 0));
+        immagini_seq1.add(new Immagine(cerchio_nero, 0));
+        immagini_seq1.add(new Immagine(quadrato_bianco, 0));
+        immagini_seq1.add(new Immagine(quadrato_nero, 0));
         immagini_risp.add(new Immagine(cerchio_nero, 0));
         immagini_risp.add(new Immagine(quadrato_mezzo, 0));
         immagini_risp.add(new Immagine(quadrato_nero, 0));
-        aggiungi();
-        immagini_seq.add(new Immagine(freccia_nero, 0));
-        immagini_seq.add(new Immagine(freccia_nero, 180));
-        immagini_seq.add(new Immagine(freccia_nero, 90));
-        immagini_seq.add(new Immagine(freccia_nero, 270));
+        aggiungi(immagini_seq1);
+        immagini_seq2.add(new Immagine(freccia_nero, 0));
+        immagini_seq2.add(new Immagine(freccia_nero, 180));
+        immagini_seq2.add(new Immagine(freccia_nero, 90));
+        immagini_seq2.add(new Immagine(freccia_nero, 270));
         immagini_risp.add(new Immagine(freccia_nero, 90));
         immagini_risp.add(new Immagine(freccia_bianco, 270));
         immagini_risp.add(new Immagine(freccia_nero, 270));
-        aggiungi();
+        aggiungi(immagini_seq2);
     }
 
-    public void svuota()
-    {
-        immagini_risp.clear();
-        immagini_seq.clear();
-        Log.d("Aaa",String.valueOf(domande.get(livello).size()));
-    }
 
-    public void aggiungi()
+    public void aggiungi(ArrayList<Immagine> immagini_seq)
     {
         Collections.shuffle(immagini_risp);
         immagini_seq.addAll(immagini_risp);
-        ArrayList<Immagine> prova;
-        prova = immagini_risp;
-        domande.add(prova);
-        svuota();
+        domande.add(immagini_seq);
     }
 
     public void controllo(View v)
