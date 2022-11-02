6b04cc26fa2dd0f66116b6b04ff9bcf67cd76082
52icube <baldinfrancesco3@gmail.com>
2022-11-02 11:42:18 +0100

    pulsanti sequenza

--- app/src/main/java/com/example/mnemosine_allena_la_memoria/Sequenze.java
+++ app/src/main/java/com/example/mnemosine_allena_la_memoria/Sequenze.java
@@ -21,17 +21,23 @@ import android.widget.TextView;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Sequenze extends AppCompatActivity {
-    private ImageView seq_1, seq_2, seq_3,risp_1,risp_2,risp_3;
+    private ImageView seq_1, seq_2, seq_3,risp_1,risp_2,risp_3,immagine2;
     private Bitmap cerchio_bianco, cerchio_nero, cerchio_mezzo, quadrato_bianco, quadrato_nero, quadrato_mezzo, triangolo_bianco, triangolo_nero, triangolo_mezzo, freccia_bianco, freccia_nero;
     private ArrayList<ImageView> sequenza = new ArrayList(), risposte = new ArrayList();
     private ArrayList<Immagine> immagini_seq = new ArrayList(), immagini_risp = new ArrayList();
     private ArrayList<ArrayList<Immagine>> domande = new ArrayList();
+    private MediaPlayer mp;
+    private static final long TEMPO = 2000;
+    private boolean volume=true;
+    private int pt_max;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +49,7 @@ public class Sequenze extends AppCompatActivity {
         risposte.add(findViewById(R.id.risp_1));
         risposte.add(findViewById(R.id.risp_2));
         risposte.add(findViewById(R.id.risp_3));
+        immagine2=findViewById(R.id.immagine2);
 
         cerchio_bianco=BitmapFactory.decodeResource(getResources(), R.drawable.cerchio_b);
         cerchio_nero=BitmapFactory.decodeResource(getResources(), R.drawable.cerchio_n);
@@ -92,38 +99,109 @@ public class Sequenze extends AppCompatActivity {
                 if(domande.get(0).get(3).getImmagine()==immagini_risp.get(0).getImmagine()&&domande.get(0).get(3).getRotazione()==immagini_risp.get(0).getRotazione())
                 {
                     Log.d("Giusto", "=Vero");
+                    giusto();
                 }
                 else
                 {
                     Log.d("Giusto", "=Falso ");
                     ImageView iv = findViewById(R.id.seq_4);
                     iv.setImageBitmap(domande.get(0).get(3).getImmagine());
+                    errore();
                 }
                 break;
             case R.id.risp_2:
                 if(domande.get(0).get(3).getImmagine()==immagini_risp.get(1).getImmagine()&&domande.get(0).get(3).getRotazione()==immagini_risp.get(1).getRotazione())
                 {
                     Log.d("Giusto", "=Vero");
+                    giusto();
                 }
                 else
                 {
                     Log.d("Giusto", "=Falso ");
                     ImageView iv = findViewById(R.id.seq_4);
                     iv.setImageBitmap(domande.get(0).get(3).getImmagine());
+                    errore();
                 }
                 break;
             case R.id.risp_3:
                 if(domande.get(0).get(3).getImmagine()==immagini_risp.get(2).getImmagine()&&domande.get(0).get(3).getRotazione()==immagini_risp.get(2).getRotazione())
                 {
                     Log.d("Giusto", "=Vero");
+                    giusto();
                 }
                 else
                 {
                     Log.d("Giusto", "=Falso ");
                     ImageView iv = findViewById(R.id.seq_4);
                     iv.setImageBitmap(domande.get(0).get(3).getImmagine());
+                    errore();
                 }
                 break;
         }
+
+    }
+
+    public void giusto(){
+        mp = MediaPlayer.create(this,R.raw.giusto);
+        immagine2.setImageBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.giusto));
+        if(volume){ mp.start();}
+        animaImmagineEsito();
+        restart();
+    }
+
+    public void errore(){
+        mp = MediaPlayer.create(this,R.raw.errore);
+        immagine2.setImageBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.sbaglio));
+        if(volume){ mp.start();}
+        animaImmagineEsito();
+        pt_max++;
+        restart();
+    }
+
+    public void restart(){
+
+
+    }
+
+    public void cambiaVolume(@NonNull View v)
+    {
+        Button b = findViewById(v.getId());
+        if(volume)
+        {
+            volume=false;
+            b.setText("VOLUME OFF");
+            b.setBackgroundColor(0xFF9E9E9E);
+        }
+        else
+        {
+            volume=true;
+            b.setText("VOLUME ON");
+            b.setBackgroundColor(0xFFFF9800);
+        }
+    }
+
+    public void animaImmagineEsito() {
+        if(immagine2.getRotationY()>=180&&immagine2.getRotationY()<360)
+        {
+            immagine2.animate().rotationY(90).setDuration(TEMPO);
+        }
+        else
+        {
+            immagine2.animate().rotationY(270).setDuration(TEMPO);
+        }
+    }
+
+    public void indietro(View v)
+    {
+        Intent intent = new Intent(Sequenze.this,SelectEsercizi.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void home(View v)
+    {
+        Intent intent = new Intent(Sequenze.this,Home.class);
+        startActivity(intent);
+        finish();
     }
 }
